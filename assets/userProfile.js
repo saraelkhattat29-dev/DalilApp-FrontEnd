6de0a3b194a13e291200a71737c38131fb55a2d7
saraelkhattat29-dev <saraelkhattat29@gmail.com>
2026-06-21 01:08:39 +0300

    اعرعرعر

--- assets/userProfile.js
+++ assets/userProfile.js
@@ -506,12 +506,12 @@ const ACTIVITY_CONFIG = {
     CreatePost: {
         dot: 'dot-green',
         svg: '<path d="M12 20h9" /><path d="M16.5 3.5a2.12 2.12 0 0 1 3 3L7 19l-4 1 1-4Z" />',
-        label: () => 'شيرت بوست جديد'
+        label: () => 'نزلت بوست جديد'
     },
     Post: {
         dot: 'dot-green',
         svg: '<path d="M12 20h9" /><path d="M16.5 3.5a2.12 2.12 0 0 1 3 3L7 19l-4 1 1-4Z" />',
-        label: () => `نشرتِ منشورًا جديدًا`
+        label: () => 'شيرت بوست جديد'
     },
     // تعليق على منشور
     Comment: {
